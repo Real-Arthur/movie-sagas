e6726bfe6f4bb50871be836e7ef926fa7de4f640
Arthur Carson <rthrcrsn@gmail.com>
2020-10-02 17:20:16 -0500

    Added pseudocode

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,6 +4,14 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
+  /*
+  SELECT *
+  FROM movies
+  JOIN movies_genres
+  ON movies.genre_id = movies_genres.movie_id
+  JOIN genres
+  ON genres.id = movies_genres.genres_id   
+  */
   res.sendStatus(500)
 });
 
